135d453f9011e78470bf51744783c1d494bec1af
Christoph Reiter <reiter.christoph@gmail.com>
2021-09-06 06:48:54 +0200

    cleanup

--- main.py
+++ main.py
@@ -43,11 +43,11 @@ qtgnjRgCIQCZHIHbCvlgg5uL8ZJQzAxLavqF2w6uUxYVrvYDj2Cqjw==
     y = public_key.public_numbers().y.to_bytes(32, "big")
     cose_key = EC2Key(crv='P_256', x=x, y=y, optional_params={'ALG': 'ES256'})
 
-    # set the key and verify the signature
+    # Set the key and verify the signature
     cose_msg.key = cose_key
     cose_msg.verify_signature()
 
-    # Now load the content and check that the checksum matches
+    # Load the content and check that the checksum matches
     import cbor2
     import hashlib
     async with httpx.AsyncClient() as client:
@@ -57,38 +57,130 @@ qtgnjRgCIQCZHIHbCvlgg5uL8ZJQzAxLavqF2w6uUxYVrvYDj2Cqjw==
     if cbor2.loads(cose_msg.payload)[2] != hashlib.sha256(content).digest():
         raise Exception()
 
-    # Now we can trust the content, so decode it
+    # We can trust the content, so decode it
     return cbor2.loads(content)
 
 
+async def certlogic(logic, data):
+    # There is no good jsonlogic/certlogic library for python,
+    # so shell out to node :(
+    import asyncio
+    import tempfile
+    import json
+    with tempfile.NamedTemporaryFile() as logic_file:
+        logic_file.write(json.dumps(logic).encode())
+        with tempfile.NamedTemporaryFile() as data_file:
+            data_file.write(json.dumps(data).encode())
+            data_file.flush()
+            logic_file.flush()
+            proc = await asyncio.create_subprocess_exec(
+                "node", "./node_modules/certlogic-js/dist/cli.js",
+                logic_file.name, data_file.name,
+                stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+            (stdout_data, stderr_data) = await proc.communicate()
+            return json.loads(stdout_data.decode())
+
+
+async def check_rules(hcert):
+    import pprint
+
+    # Check if the HCERT can be used to go out and get a beer
+    # This is done by combining the HCERT JSON with a so called
+    # "Value Set" and the current time, then take a "Business Rule" and pass both
+    # through an extension of JsonLogic called CertLogic.
+    # If that process returns true for all rules then we can have a beer.
+    valusets = await fetch_austria_data_and_verify("valuesets")
+    rules = await fetch_austria_data_and_verify("rules")
+    #pprint.pprint(rules)
+    #pprint.pprint(valusets)
+
+    # https://jsonlogic.com/
+    # certlogic: https://github.com/ehn-dcc-development/dgc-business-rules/blob/main/certlogic/specification/README.md
+
+    # convert the valuesets to something json/certlogic can work with
+    import json
+    value_sets_for_logic = {}
+    for entry in valusets["v"]:
+        v = json.loads(entry["v"])
+        value_sets_for_logic[v["valueSetId"]] = list(v["valueSetValues"].keys())
+
+    # filter the rules so we only get the ones for Austria and only "Eintrittstest"
+    filtered_rules = []
+    for entry in rules["r"]:
+        r = json.loads(entry["r"])
+        if r["Country"] == "AT" and r["Region"] == "ET":
+            filtered_rules.append(r)
+    #pprint.pprint(filtered_rules[1]["Description"])
+    #pprint.pprint(filtered_rules[1]["Logic"])
+
+    # Create the required input for certlogic
+    from datetime import datetime, timezone
+    validationClock = datetime.now(timezone.utc).isoformat()
+    logic_data = {
+        "payload": hcert,
+        "external": {
+            "valueSets": value_sets_for_logic,
+            "validationClock": validationClock
+        },
+    }
+    #pprint.pprint(logic_data, depth=3)
+
+    # Check our input against all rules
+    results = []
+    for r in filtered_rules:
+        logic_result = await certlogic(r["Logic"], logic_data)
+        results.append((r, logic_result))
+
+    # Now we print the result
+    ok = True
+    for rule, result in results:
+        print(f"{'✅' if result else '❌'} [{rule['Identifier']}] {rule['Description'][0]['desc']}")
+        if not result:
+            ok = False
+    print("~" * 40)
+    print(("🍻" if ok else "❌") + " " + ("No beer for you" if not ok else "Cheers"))
+
+
 async def main(filename: str):
+    import pprint
+
     # We convert the input PDF to a PIL image
     from pdf2image import convert_from_path
     image = convert_from_path(filename)[0]
+    #image.show()
 
     # We scan the image for a QR code
     from pyzbar.pyzbar import decode
     payload = decode(image)[0]
     assert payload.data.startswith(b"HC1:")
+    #pprint.pprint(payload.data)
 
     # The payload is prefixed with "HC1:" -> strip and base45 decode
     import base45
     payload = base45.b45decode(payload.data[4:].decode())
+    #pprint.pprint(payload)
 
     # zlib decode
     import zlib
     payload = zlib.decompress(payload)
+    #pprint.pprint(payload)
 
     # Now we have a signed CBOR data structure -> COSE message
     # Note: COSE is to CBOR what JOSE (JWT) is to JSON
     from cose.messages import CoseMessage
     cose_msg = CoseMessage.decode(payload)
+    #pprint.pprint({
+    #    "uhdr": cose_msg.uhdr,
+    #    "pdhr": cose_msg.phdr,
+    #    "signature": cose_msg.signature,
+    #    "payload": cose_msg.payload})
 
     # We extract the KID of the certificate we need to verify the signature
     import cose.headers
     required_kid = cose_msg.get_attr(cose.headers.KID)
+    #pprint.pprint(required_kid)
 
-    # Now to verify the signature we have to find the right certifcate
+    # To verify the signature we have to find the right certifcate
     # The Austrian government provides an API:
     trustlist = await fetch_austria_data_and_verify("trustlist")
     for entry in trustlist["c"]:
@@ -99,18 +191,21 @@ async def main(filename: str):
     else:
         raise Exception("kid nto found")
     found_cert = cert
+    #pprint.pprint(found_cert)
 
-    # Now we convert to CERT to a COSE key
-    # WARNING: we assume ES256 here but all other algorithms are allowed too
+    # Parse the x509 cert
     from cryptography import x509
     from cose.keys import EC2Key
     cert = x509.load_der_x509_certificate(found_cert)
+    #pprint.pprint(cert)
+
+    # Convert the CERT to a COSE key and verify teh signature
+    # WARNING: we assume ES256 here but all other algorithms are allowed too
+    assert cose_msg.get_attr(cose.headers.Algorithm).fullname == "ES256"
     public_key = cert.public_key()
     x = public_key.public_numbers().x.to_bytes(32, "big")
     y = public_key.public_numbers().y.to_bytes(32, "big")
-    cose_key = EC2Key(crv='P_256', x=x, y=y, optional_params={'ALG': 'ES256'})
-
-    # Now we verify the signature
+    cose_key = EC2Key(crv='P_256', x=x, y=y)
     cose_msg.key = cose_key
     cose_msg.verify_signature()
 
@@ -118,67 +213,16 @@ async def main(filename: str):
     # https://ec.europa.eu/health/sites/default/files/ehealth/docs/covid-certificate_json_specification_en.pdf
     import cbor2
     import pprint
-    hcert = cbor2.loads(cose_msg.payload)[-260][1]
-    pprint.pprint(hcert)
+    payload = cbor2.loads(cose_msg.payload)
+    #pprint.pprint(payload)
 
-    # Now we need to check if the HCERT can be used to go out and get a beer
-    # This is done by combining the HCERT JSON with a so called
-    # "Value Set" and the current time, then take a "Business Rule" and pass both
-    # through an extension of JsonLogic called CertLogic.
-    # If that process returns true for all rules then we can have a beer.
-    valusets = await fetch_austria_data_and_verify("valuesets")
-    rules = await fetch_austria_data_and_verify("rules")
+    # Get the inner content
+    hcert = payload[-260][1]
+    #pprint.pprint(hcert)
 
-    # convert the valuesets to something json/certlogic can work with
-    import json
-    value_sets_for_logic = {}
-    for entry in valusets["v"]:
-        v = json.loads(entry["v"])
-        value_sets_for_logic[v["valueSetId"]] = list(v["valueSetValues"].keys())
-
-    filtered_rules = []
-    for entry in rules["r"]:
-        r = json.loads(entry["r"])
-        # Only for Austria and only "Eintrittstest"
-        if r["Country"] == "AT" and r["Region"] == "ET":
-            filtered_rules.append(r)
-
-    from datetime import datetime, timezone
-
-    validationClock = datetime.now(timezone.utc).isoformat()
-    logic_data = {
-        "payload": hcert,
-        "external": {
-            "valueSets": value_sets_for_logic,
-            "validationClock": validationClock
-        },
-    }
-
-    def certlogic(logic, data):
-        # there is no good jsonlogic/certlogic library for python,
-        # so shell out to node :(
-        import subprocess
-        import tempfile
-        import json
-        with tempfile.NamedTemporaryFile() as logic_file:
-            logic_file.write(json.dumps(logic).encode())
-            with tempfile.NamedTemporaryFile() as data_file:
-                data_file.write(json.dumps(data).encode())
-                data_file.flush()
-                logic_file.flush()
-                return json.loads(subprocess.check_output(["node", "./node_modules/certlogic-js/dist/cli.js", logic_file.name, data_file.name], text=True))
-
-    ok = True
-    for r in filtered_rules:
-        result = certlogic(r["Logic"], logic_data)
-        if not result:
-            ok = False
-        print(f"{'✅' if result else '❌'} [{r['Identifier']}] {r['Description'][0]['desc']}")
-
-    print("~" * 40)
-    print(("🍻" if ok else "❌") + " " + ("No beer for you" if not ok else "Cheers"))
+    await check_rules(hcert)
 
 
 if __name__ == "__main__":
     import asyncio
-    asyncio.run(main("input1.pdf"))
+    asyncio.run(main("input3.pdf"))
